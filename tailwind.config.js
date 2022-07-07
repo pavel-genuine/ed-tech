1a9656a3b8db56b05457cffad6c3a665b8b426ca
pavel.genuine <pavel.play@gmail.com>
2022-07-07 18:47:30 +0600

    all ok

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,7 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
     theme: {
     extend: {
-      backgroundImage: {
-        'hero-pattern': "url('https://img.freepik.com/free-photo/islamic-new-year-concept-with-copy-space_23-2148611773.jpg?t=st=1656139594~exp=1656140194~hmac=5a999a8c9ab18f8314cb6cd00a7cc3078862c9ff1a9824fad8343dc26830f689&w=1380')",
-        'footer-texture': "url('/img/footer-texture.png')",
-      }
+
     },
   },
   plugins: [require("daisyui")],}
